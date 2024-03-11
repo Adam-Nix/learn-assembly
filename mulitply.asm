da2e4092920117e690f7191c7404919375aa7ca3
Adam Nix <150264616+realadamnix@users.noreply.github.com>
2024-03-11 15:35:15 +0000

    multiply

--- mulitply.asm
+++ mulitply.asm
@@ -1,7 +1,18 @@
-                INP
-                STA varMultiplicand
-                INP
-                STA varMultiplier
+///
+///  Multiply. This program expects 2 postive numbers as input and outputs their product.
+///  If the input is 0, the output is 0.
+///
+lbInput1        INP
+                BRZ lbInput2
+                BRP lbSave1
+                BRA lbInput1
+lbSave1         STA varMultiplicand
+lbInput2        INP
+                BRZ lbOutput        // if input is 0, output 0
+                BRP lbSave2
+                BRA lbInput2
+
+lbSave2         STA varMultiplier
 lbMultiplyLoop  LDA varProduct
                 ADD varMultiplicand
                 STA varProduct 
